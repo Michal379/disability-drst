bf2546df3f0c3a9c947e2cf07069521cf3c3285f
Michal <khakaimic4@gmail.com>
2023-08-13 02:23:55 +0300

    header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -6,25 +6,25 @@ import './Header.css';
 
 function Header() {
     return (
-        <footer className="footer navbar navbar-expand-lg bg-success mt-3 fixed-bottom" style={{ position: 'sticky', bottom: 0, zIndex: 999 }}>
+        <header className="header navbar navbar-expand-lg bg-success mt-3 fixed-top" style={{ position: 'sticky', top: 0, zIndex: 999 }}>
             <div className="container">
-                <div className="footer-content d-flex justify-content-between align-items-center">              
-                    <div className="footer-right d-flex flex-column align-items-center" style={{ marginLeft: '20px' }}>
+                <div className="header-content d-flex justify-content-between align-items-center">
+                    <div className="header-right d-flex flex-column align-items-center" style={{ marginLeft: '20px' }}>
                         <div className="social-links">
-                               {/* Phone */}
+                            {/* Phone */}
                             <a
                                 href="tel:+1234567890"
                                 className="social-link bg-blue text-white"
                             >
-                                <FontAwesomeIcon icon={faPhone} />
+                                <FontAwesomeIcon icon={faPhone} /> (+255) 620 583-841
                             </a>
 
                             {/* Email */}
                             <a
-                                href="mailto:contact@example.com"
+                                href="mailto:info@drst.co.tz"
                                 className="social-link bg-blue text-white"
                             >
-                                <FontAwesomeIcon icon={faEnvelope} />
+                                <FontAwesomeIcon icon={faEnvelope} /> info@drst.co.tz
                             </a>
 
                             {/* Location */}
@@ -34,18 +34,20 @@ function Header() {
                                 rel="noopener noreferrer"
                                 className="social-link bg-blue text-white"
                             >
-                                <FontAwesomeIcon icon={faMapMarker} />
+                                <FontAwesomeIcon icon={faMapMarker} /> Murubona, Kasulu, Kigoma Tanzania
                             </a>
-                            
-    {/* YouTube */}
-    <a
-        href="https://www.youtube.com/your-youtube-channel"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="social-link bg-blue text-white"
-    >
-        <FontAwesomeIcon icon={faYoutube} />
-    </a>
+
+                            {/* YouTube */}
+                            <a
+                                href="https://www.youtube.com/your-youtube-channel"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="social-link bg-blue text-white"
+                            >
+                                <FontAwesomeIcon icon={faYoutube} />
+                            </a>
+
+                            {/* Other social media links */}
                             <a
                                 href="https://www.facebook.com/yourfacebookpage"
                                 target="_blank"
@@ -90,7 +92,7 @@ function Header() {
                     </div>
                 </div>
             </div>
-        </footer>
+        </header>
     );
 }
 
