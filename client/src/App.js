9a640d5573f48000261cb849e1772277a3af03be
Michal <khakaimic4@gmail.com>
2023-08-14 01:30:46 +0300

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Navbar />
       <Footer />
       </div>
   )
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css'; // Import your CSS file for styling
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a href="#" className="nav-link">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="#who-we-are" className="nav-link">Who We Are</a>
+        </li>
+        <li className="nav-item">
+          <a href="#what-we-do" className="nav-link">What We Do</a>
+        </li>
+        <li className="nav-item">
+          <a href="#our-projects" className="nav-link">Our Projects</a>
+        </li>
+        <li className="nav-item">
+          <a href="#our-partners" className="nav-link">Our Partners</a>
+        </li>
+        <li className="nav-item">
+          <a href="#contact-us" className="nav-link">Contact Us</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
