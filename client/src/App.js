57756c5a7c0b8b3c99139f415c78a13b7c3b9598
Michal <khakaimic4@gmail.com>
2023-08-13 13:02:18 +0300

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import Header from './components/header/Header'
-
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-    </div>
+    <div>App</div>
   )
 }
 
