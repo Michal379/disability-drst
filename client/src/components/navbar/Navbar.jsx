8fa804dae39bdb37652e54cb4824f2cdaf9b0b60
Michal <khakaimic4@gmail.com>
2023-08-14 10:11:41 +0300

    nabar styling

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Import the CSS file
 
 const Navbar = () => {
+  const navStyle = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    background: 'gray',
+    color: '#fff',
+    padding: '1rem 0',
+  };
+
+  const linkStyle = {
+    color: 'black',
+    textDecoration: 'none',
+    padding: '0.5rem 1rem',
+    borderRadius: '5px',
+  };
+
   return (
-    <nav className="navbar">
-      <ul className="navbar-nav">
+    <nav style={navStyle}>
+      <ul style={{ listStyleType: 'none', display: 'flex', padding: 0 }}>
         <li className="nav-item">
-          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/" style={linkStyle}>Home</Link>
         </li>
         <li className="nav-item">
-          <Link to="/who-we-are" className="nav-link">Who We Are</Link>
+          <Link to="/who-we-are" style={linkStyle}>Who We Are</Link>
         </li>
         <li className="nav-item">
-          <Link to="/what-we-do" className="nav-link">What We Do</Link>
+          <Link to="/what-we-do" style={linkStyle}>What We Do</Link>
         </li>
         <li className="nav-item">
-          <Link to="/our-projects" className="nav-link">Our Projects</Link>
+          <Link to="/our-projects" style={linkStyle}>Our Projects</Link>
         </li>
         <li className="nav-item">
-          <Link to="/our-partners" className="nav-link">Our Partners</Link>
+          <Link to="/our-partners" style={linkStyle}>Our Partners</Link>
         </li>
         <li className="nav-item">
-          <Link to="/contact-us" className="nav-link">Contact Us</Link>
+          <Link to="/contact-us" style={linkStyle}>Contact Us</Link>
         </li>
       </ul>
     </nav>
