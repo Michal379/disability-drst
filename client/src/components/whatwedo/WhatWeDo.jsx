e2471ac032435b12257507fbdcbe1dd804616e63
Michal <khakaimic4@gmail.com>
2023-08-14 20:58:18 +0300

    who we are

--- client/src/components/whatwedo/WhatWeDo.jsx
+++ client/src/components/whatwedo/WhatWeDo.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const WhatWeDo = () => {
   return (
-    <div>WhatWeDo</div>
+    <div>
+      <h2>WhatWeDo</h2>
+      <p>Most of the projects, programs and activities undertaken by the organization generally</p>
+      <p>associated with efforts in People with Special Needs programs.</p>
+    </div>
   )
 }
 
--- client/src/components/whoweare/WhoWeAre.jsx
+++ client/src/components/whoweare/WhoWeAre.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const WhoWeAre = () => {
   return (
-    <div>WhoWeAre</div>
+    <div>
+      <h2>WhoWeAre</h2>
+      <p>DISABILITY RELIEF SERVICES (DRS) TANZANIA is a nonprofit making organization dealing with</p>
+      <p>improvement of socio-economic condition in vulnerable, marginalized and deprived communities in Tanzania.</p>
+      <p>and Health. Core activities: Social Protection Interventions for People with Special Needs (PWSNs) and</p>
+      <p>adolescent girls and young women (AGYW), advocacy and promotion for human rights, actions to end Violence</p>
+      <p>Against Women and Children (VAWC), Social economic empowerment, Environment and Health.</p>
+      <button>Learn More</button>      
+    </div>
   )
 }
 
