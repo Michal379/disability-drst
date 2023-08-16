53e979f3c2f5aa59d9c5eb0a0ccc9360b100402a
Michal <khakaimic4@gmail.com>
2023-08-16 22:16:48 +0300

    footer styling

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -26,11 +26,12 @@ const Footer = () => {
         <p>A nonprofit making organization dealing with improvement of socio-economic condition in vulnerable, marginalized and deprived communities in Tanzania.</p>
       </div>
       <div className="footer-section footer-copyright">
-        <div className="copyright-content">
-          <p>&copy; 2023</p>
-          <p>&copy; All Rights Reserved</p>
-        </div>
-      </div>
+  <div className="copyright-content">
+    <p className="left-copyright">&copy; 2023</p>
+    <p className="right-copyright">&copy; All Rights Reserved</p>
+  </div>
+</div>
+
     </div>
   );
 }
